69f64e40cbfc37aea245756cd49f84eeeaf785ad
Zilpan <137197001+Zilpan@users.noreply.github.com>
2026-03-10 23:30:20 +0300

    Homework 2: Changed Gradle version (again)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.projectone"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.projectone"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 30
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
